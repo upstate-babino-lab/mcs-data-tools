e388e61f36ef6558900896c0a5d9850e8b377763
Pierre Wellner <pierre@pierrewellner.com>
2025-04-14 22:53:59 -0400

    Check PDA data in addition to audio

--- locate_synctones.py
+++ locate_synctones.py
@@ -2,7 +2,7 @@
 #
 # With a .h5 file exported from Multi Channel Systems DataManager
 # extract the audio data from the analog-in-1 channel, and locate
-# the precise timestamp of each tone.
+# the precise timestamp of each synctone.
 #
 
 import argparse
@@ -11,12 +11,12 @@ import numpy as np
 from scipy.signal import savgol_filter, find_peaks
 from sklearn.preprocessing import MinMaxScaler, RobustScaler
 from h5_tools import get_date_and_duration, valid_filename
-from plot_data import plot_audio_data
+from plot_data import plot_data
 
 ANALOG_1_GROUP = "/Data/Recording_0/AnalogStream/Stream_3"
 
 
-def get_audio_data(file_path):
+def get_analog_data(file_path, desiredLabelStr):
     with h5py.File(file_path, "r") as f:
         group = f[ANALOG_1_GROUP]
 
@@ -34,26 +34,27 @@ def get_audio_data(file_path):
                 f"Expected 'ChannelData' dataset not found in group {ANALOG_1_GROUP}"
             )
 
-        # If there are several analog channels, find the one with 'audio' in the label
+        # Find the channel with {desiredLabelStr} in the label
         info = group["InfoChannel"][()]
         nAnalogChannels = info.shape[0]
         print(f"Number of analog channels = {nAnalogChannels}")
-        audioChannelNumber = 0
+        labeledChannelNumber = -1
         if nAnalogChannels > 0:
             for i in range(nAnalogChannels):
-                if (
-                    "left" in info["Label"][i].decode("utf-8").lower()
-                    or "audio" in info["Label"][i].decode("utf-8").lower()
-                ):
-                    audioChannelNumber = i
+                if desiredLabelStr in info["Label"][i].decode("utf-8").lower():
+                    labeledChannelNumber = i
+        if labeledChannelNumber < 0:
+            raise Exception(
+                f"Unable to find channel with label '{desiredLabelStr}' in {ANALOG_1_GROUP}"
+            )
 
-        label = info["Label"][audioChannelNumber].decode("utf-8")
-        print(f"Using channel {audioChannelNumber} with label = '{label}'")
+        label = info["Label"][labeledChannelNumber].decode("utf-8")
+        print(f"Using channel {labeledChannelNumber} with label = '{label}'")
 
-        microseconds_between_samples = info["Tick"][audioChannelNumber]
+        microseconds_between_samples = info["Tick"][labeledChannelNumber]
         data_rate = round(1_000_000 / microseconds_between_samples)
 
-        analog_data = group["ChannelData"][audioChannelNumber, :]
+        analog_data = group["ChannelData"][labeledChannelNumber, :]
         print("Analog data shape:", analog_data.shape)
         duration_seconds = len(analog_data) / data_rate
         print(
@@ -61,9 +62,50 @@ def get_audio_data(file_path):
             + f" duration={round(duration_seconds)} seconds ={duration_seconds / 60:.2f} minutes"
         )
         return (
-            analog_data.reshape(-1, 1),
+            analog_data.reshape(-1, 1),  # Reshape to single-column 2D array
             data_rate,
-        )  # Reshape to single-column 2D array
+        )
+
+
+def locate_synctones(file_path, do_plot=False):
+    audio_data, _ = get_analog_data(file_path, "audio")
+    scaler = MinMaxScaler()
+    scaled = scaler.fit_transform(audio_data)
+    centered = audio_data - np.mean(audio_data)
+    squared = scaler.fit_transform(centered**2)
+    smoothed = scaler.fit_transform(
+        savgol_filter((centered**2).flatten(), 1800, 3).reshape(audio_data.shape)
+    )
+    peaks, _ = find_peaks(smoothed.flatten(), height=0.5, distance=2000)
+    if do_plot:
+        min = peaks[0] - 2000
+        max = peaks[0] + 2000
+        peaks_in_range = peaks[(peaks > min) & (peaks < max)]
+        plot_data(
+            scaled[min:max], squared[min:max], smoothed[min:max], peaks_in_range - min
+        )
+
+    print(f"Found {len(peaks)} peaks")
+    diffs = (10_000 - np.diff(peaks)) / 10  # Diffs in milliseconds
+    mean_value = np.mean(diffs)
+    min_value = np.min(diffs)
+    max_value = np.max(diffs)
+    std_dev = np.std(diffs)
+    print(
+        f"Millisecond diffs mean: {mean_value:.2f} min: {min_value} max: {max_value} stdDev: {std_dev:.2f}"
+    )
+    return peaks / 10_000  # Timestamps in seconds
+
+
+def locate_pda_transitions(file_path, do_plot=False):
+    pda_data, _ = get_analog_data(file_path, "pda")
+    scaler = MinMaxScaler()
+    scaled = scaler.fit_transform(pda_data)
+
+    if do_plot:
+        min = 1000
+        max = 30000
+        plot_data(pda_data[min:max])
 
 
 def main():
@@ -74,37 +116,10 @@ def main():
     args = parser.parse_args()
     get_date_and_duration(args.filename)
 
-    audio_data, data_rate = get_audio_data(args.filename)
+    synctone_timestamps = locate_synctones(args.filename, args.plot)
+    print(synctone_timestamps)
 
-    scaler = MinMaxScaler()
-    scaled = scaler.fit_transform(audio_data)
-    centered = audio_data - np.mean(audio_data)
-    squared = scaler.fit_transform(centered**2)
-    smoothed = scaler.fit_transform(
-        savgol_filter((centered**2).flatten(), 1800, 3).reshape(audio_data.shape)
-    )
-    peaks, _ = find_peaks(smoothed.flatten(), height=0.5, distance=2000)
-    if args.plot:
-        min = peaks[0] - 1500
-        max = peaks[0] + 1500
-        peaks_in_range = peaks[(peaks > min) & (peaks < max)]
-        plot_audio_data(
-            scaled[min:max], squared[min:max], smoothed[min:max], peaks_in_range - min
-        )
-    print(f"Found {len(peaks)} peaks")
-    diffs = (10_000 - np.diff(peaks)) / 10  # Diffs in milliseconds
-    mean_value = np.mean(diffs)
-    min_value = np.min(diffs)
-    max_value = np.max(diffs)
-    std_dev = np.std(diffs)
-
-    # print(f"Diffs: {diffs}")
-    print(
-        f"Millisecond diffs mean: {mean_value:.2f} min: {min_value} max: {max_value} stdDev: {std_dev:.2f}"
-    )
-
-    print("SyncTone locations (seconds):")
-    print(peaks / 10_000)  # Timestamps in seconds
+    pda_timestamps = locate_pda_transitions(args.filename, args.plot)
 
 
 if __name__ == "__main__":
--- plot_data.py
+++ plot_data.py
@@ -4,10 +4,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plot_audio_data(raw, squared=None, smoothed=None, peaks=None):
-
+def plot_data(raw, squared=None, smoothed=None, peaks=None):
     x = np.arange(0, len(raw))
-
     plt.figure(figsize=(12, 4))
     plt.plot(x, raw, label="Raw", alpha=0.4)
     if squared is not None:
